06b6dbdc3ad2e00469da20c5fb141fdd591bdbf4
guswl458@gmail.com <guswl458@gmail.com>
2024-12-05 10:29:16 +0900

    20241205

---
+++ crawling/src/service/tms.py
@@ -0,0 +1,86 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+def run():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://koritbs.cafe24.com/student/index.php")
+    sleep(2)
+
+    loginId= driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > div > form > table > tbody > tr:nth-child(3) > td > input"
+    )
+
+    loginId.send_keys("guswlwldhks")
+    sleep(2)
+
+    loginPassword = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > div > form > table > tbody > tr.border-left-danger.border-right-danger.border-bottom-0.border-top-0.bg-brown > td > input"
+    )
+    loginPassword.send_keys("26815282")
+    sleep(2)
+
+    loginButton = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > div > form > table > tbody > tr.border-left-danger.border-right-danger.border-bottom-danger.border-top-0.bg-brown > td > div > div:nth-child(2) > button"
+    )
+    loginButton.click()
+    sleep(5)
+
+    ncs = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr.hover.pointer > td:nth-child(4)"
+    )
+    ncs.click()
+    sleep(2)
+
+    crawlingButton = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > li:nth-child(10) > div"
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", crawlingButton)
+    driver.execute_script("arguments[0].click();", crawlingButton)
+
+    sleep(2)
+
+    boardButton = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > caption > div > div:nth-child(2) > div > a"
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", boardButton)
+    driver.execute_script("arguments[0].click();", boardButton)
+
+    sleep(2)
+
+    BoardInput = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > input"
+    )
+    BoardInput.send_keys("황현지")
+
+    contentInput = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > div.ck.ck-editor__main > div"
+    )
+    contentInput.send_keys("황현지")
+    sleep(2)
+
+
+    okButton = driver.find_element(
+        by=By.CSS_SELECTOR,
+        value="body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr.end > td > button"
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", okButton)
+    driver.execute_script("arguments[0].click();", okButton)
+    sleep(2)
+
+
+
+
--- crawling/src/service/webtoon.py
+++ crawling/src/service/webtoon.py
@@ -1,27 +1,86 @@
+import json
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from src.repository.webtoon_repository import saveWebtoonDataList, saveAuthor
 
 def run():
+    webtoonDataList = [
+        {
+            "categoryName": "월",
+            "webtoons": [
+                {
+                    "title": "참교육",
+                    "author": "채용택 / 한가람",
+                    "rating": 9.89,
+                    "imgUrl": "https://~~~~~~"
+                },
+                {
+                    "title": "똑 닮은 딸",
+                    "author": "이담",
+                    "rating": 9.98,
+                    "imgUrl": "https://~~~~~~"
+                }
+            ]
+        }
+    ]
+
+    webtoonDataList.clear()
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
     driver.get("https://comic.naver.com/webtoon")
+    driver.maximize_window()
     sleep(1)
 
-    days=driver.find_elements(by=By.CSS_SELECTOR,
-                         value='#wrap > header > div.SubNavigationBar__snb_wrap--A5gfM > nav > ul > li > a')
-    for day in days[1:8]:
-        driver.execute_script("arguments[0].scrollIntoView(true);", day)
-        day.click()
+    categoryLis = driver.find_elements(
+        by=By.CSS_SELECTOR,
+        value='#wrap > header > div.SubNavigationBar__snb_wrap--A5gfM > nav > ul > li'
+    )
 
-        print(day.text)
+    for categoryLi in categoryLis[1:3]:
+        categoryLink = categoryLi.find_element(by=By.CSS_SELECTOR, value="& > a")
+        categoryLink.click()
+        sleep(0.5)
 
-        webtoonTitles = driver.find_elements(by=By.CSS_SELECTOR,
-                                             value='#content > div:nth-child(1) > ul > li > div > a > span')
+        webtoonDataOfCategory = {
+            "categoryName": categoryLink.text,
+            "webtoons": []
+        }
 
-        for webtoonTitle in webtoonTitles:
-            print(webtoonTitle.text)
+        webtoonLis = driver.find_elements(by=By.CSS_SELECTOR, value="#content > div:nth-child(1) > ul > li")
+        for webtoonLi in webtoonLis:
+            driver.execute_script("arguments[0].scrollIntoView(true);", webtoonLi)
+            img = webtoonLi.find_element(by=By.CSS_SELECTOR, value="& > a > div > img")
+            imgSrc = img.get_attribute("src")
+            title = webtoonLi.find_element(by=By.CSS_SELECTOR, value="& > div > a:nth-of-type(1)")
+            titleText = title.text
+            author = webtoonLi.find_element(by=By.CSS_SELECTOR, value="& > div .ContentAuthor__author--CTAAP")
+            authorText = author.text
+            rating = webtoonLi.find_element(by=By.CSS_SELECTOR, value="& > div > div:nth-last-of-type(1) > span > span")
+            ratingText = rating.text
+            webtoon = {
+                "title": titleText,
+                "author": authorText,
+                "rating": float(ratingText),
+                "imgUrl": imgSrc
+            }
+            webtoonDataOfCategory["webtoons"].append(webtoon)
+        webtoonDataList.append(webtoonDataOfCategory)
+
+    print(webtoonDataList)
+    saveWebtoonDataList(webtoonDataList)
+    saveAuthor(webtoonDataList)
+
+
+
+
+
+
+
+
 
 def run2():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -35,9 +94,102 @@ def run2():
         day.click()
 
         print(day.text)
+        sleep(0.1)
 
         webtoonTitles = driver.find_elements(by=By.CSS_SELECTOR,
                                              value='#container > div.ListSpot__spot_wrap--Iko15 > div.content > div > ul > li > div > a > span > span')
 
         for webtoonTitle in webtoonTitles:
             print(webtoonTitle.text)
+
+def run3():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://kr.stussy.com/collections/new-arrivals")
+    driver.maximize_window()
+    sleep(1)
+
+    categoryies = driver.find_elements(
+        by = By.CSS_SELECTOR,
+        value='#dropdown-menu-1-shop > div > ul > li > a'
+    )
+
+    for i in range(len(categoryies)):
+        categoryies = driver.find_elements(
+            by = By.CSS_SELECTOR,
+            value='#dropdown-menu-1-shop > div > ul > li > a'
+        )
+        categoryName = categoryies[i].text
+        print(f"카테고리: {categoryName}")
+
+        driver.execute_script("arguments[0].scrollIntoView(true);", categoryies[i])
+        driver.execute_script("arguments[0].click();", categoryies[i])
+        sleep(0.1)
+
+
+        productLis = driver.find_elements(
+            by = By.CSS_SELECTOR,
+            value='#shopify-section-template--14469189140535__product-grid > s-collection-grid > div.collection-grid__layout.px-sm.pt-md.pb-xl.tabletp\:px-md > ul > li'
+        )
+        # #'#shopify-section-template--14469189140535__product-grid > s-collection-grid > div.collection-grid__layout.px-sm.pt-md.pb-xl.tabletp\:px-md > ul > li:nth-child(4) > div > div'
+        for productLi in productLis[:4]:
+            productImg = productLi.find_element(by=By.CSS_SELECTOR, value='div > a > div > img')
+            productImgSrc = productImg.get_attribute('src')
+            productName =  productLi.find_element( by= By.CSS_SELECTOR, value= 'div > div > div:nth-of-type(1)')
+            productNameText = productName.text
+            productPrice = productLi.find_element(by=By.CSS_SELECTOR, value= 'div > div > div:nth-of-type(2)')
+            productPriceText = productPrice.text
+            print(f'상품명: {productNameText}, 가격: {productPriceText}, url: {productImgSrc}')
+
+def run4():
+    products = []
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    driver.get("https://kr.stussy.com/collections/new-arrivals")
+    driver.maximize_window()
+    sleep(1)
+
+    categoryies = driver.find_elements(
+        by=By.CSS_SELECTOR,
+        value='#dropdown-menu-1-shop > div > ul > li > a'
+    )
+
+    for i in range(len(categoryies)):
+        categoryies = driver.find_elements(
+            by=By.CSS_SELECTOR,
+            value='#dropdown-menu-1-shop > div > ul > li > a'
+        )
+        categoryName = categoryies[i].text
+        print(f"카테고리: {categoryName}")
+
+        categoryDict = {
+            "categoryName": categoryName,
+            "productList": []
+        }
+
+        driver.execute_script("arguments[0].scrollIntoView(true);", categoryies[i])
+        driver.execute_script("arguments[0].click();", categoryies[i])
+        sleep(0.1)
+
+        productLis = driver.find_elements(
+            by=By.CSS_SELECTOR,
+            value='#shopify-section-template--14469189140535__product-grid > s-collection-grid > div.collection-grid__layout.px-sm.pt-md.pb-xl.tabletp\:px-md > ul > li'
+        )
+        # #'#shopify-section-template--14469189140535__product-grid > s-collection-grid > div.collection-grid__layout.px-sm.pt-md.pb-xl.tabletp\:px-md > ul > li:nth-child(4) > div > div'
+        for productLi in productLis[:4]:
+            productImg = productLi.find_element(by=By.CSS_SELECTOR, value='div > a > div > img')
+            productImgSrc = productImg.get_attribute('src')
+            productName = productLi.find_element(by=By.CSS_SELECTOR, value='div > div > div:nth-of-type(1)')
+            productNameText = productName.text
+            productPrice = productLi.find_element(by=By.CSS_SELECTOR, value='div > div > div:nth-of-type(2)')
+            productPriceText = productPrice.text
+            print(f'상품명: {productNameText}, 가격: {productPriceText}, url: {productImgSrc}')
+            categoryDict["productList"].append({
+                "productName": productNameText,
+                "productPrice": productPriceText,
+                "productImgSrc": productImgSrc,
+            })
+
+        products.append(categoryDict)
+    with open('products.json', 'w', encoding='utf-8') as f:
+        json.dump(products, f, ensure_ascii=False, indent=4)
